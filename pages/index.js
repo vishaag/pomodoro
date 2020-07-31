94f5c2d3c356d4debfef3fbd33c865f599c74f9d
vishaag <vishaag@gmail.com>
2020-07-31 18:17:25 +0800

    Start, Pause, Reset func.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,76 @@
+import { useState, useRef } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+function padTime(time) {
+  return time.toString().padStart(2,'0')
+}
+
 export default function Home() {
+  const [title, setTitle] = useState('Let the countdown begin!');
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const minutes = padTime(Math.floor(timeLeft/60));
+  const seconds = padTime(timeLeft - minutes*60);
+
+  function startTimer() {
+    if(intervalRef.current !== null) return;
+
+    setTitle(`You're doing great!`)
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if(timeLeft>=1) return timeLeft - 1;
+
+        resetTimer();
+        return 0;
+      })
+    }, 1000)
+  }
+
+  function stopTimer() {
+    if(intervalRef.current === null) return;
+
+    setIsRunning(false);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up!');
+  }
+
+  function resetTimer() {
+    setIsRunning(false);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Ready for another round?');
+    setTimeLeft(25 * 60);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pomodoro Timer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <div className={styles.app}>
+          <h2>{title}</h2>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <div className={styles.timer}>
+            <span>{minutes}</span>
+            <span>:</span>
+            <span>{seconds}</span>
+          </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.buttons}>
+            {!isRunning && <button onClick={startTimer}>Start</button>}
+            {isRunning && <button onClick={stopTimer}>Pause</button>}
+            <button onClick={resetTimer}>Reset</button>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
